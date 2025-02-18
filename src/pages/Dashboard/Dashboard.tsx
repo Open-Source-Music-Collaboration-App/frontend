2753ee4f93b574f7938a597eb60d8ae08df3816a
Sooraj Gupta <soorajgupta04@gmail.com>
2025-02-18 01:38:36 -0600

    load user projects added

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,21 +1,40 @@
 import React, { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import Sidebar from "../../components/Sidebar/Sidebar";
-import "./Dashboard.css";
 import { useAuth } from "../../context/AuthProvider";
+import "./Dashboard.css";
 
 function Dashboard() {
     const { user } = useAuth();
+    const [projects, setProjects] = useState([]);
+
+    useEffect(() => {
+        if (user) {
+            console.log("Fetching projects for user:", user);
+            axios.get(`http://localhost:3333/api/user_projects/${user.id}`, { withCredentials: true })
+                .then(response => setProjects(response.data))
+                .catch(error => console.error("Error fetching projects:", error));
+        }
+    }, [user]);
 
     return (
         <div className="dashboard-container">
-            <div>
-                <h1>Dashboard</h1>
-            </div>
+            <ul>
+                {projects.map((project) => (
+                    <li key={project.id}>
+                        <h3>{project.title}</h3>
+                        {/* Month Day, Year */}
+                        <p>{project.createdBy} Last Updated: {new Date(project.updatedAt).toLocaleDateString()}</p>
+                        {project.hashtags && (
+                            <p>Tags: {project.hashtags.join(", ")}</p>
+                        )}
+                        {/* <button className = "buttonoutline" onClick={() => window.location.href = `/project/${project.id}`}>
+                            View Project
+                        </button> */}
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 }
 
 export default Dashboard;
-
