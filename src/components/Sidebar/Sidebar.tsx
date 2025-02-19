5ef88d5ee1cac992ed3aeb3c1dc223d27848c29b
Sooraj Gupta <soorajgupta04@gmail.com>
2025-02-19 17:34:30 -0600

    minor changes

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -2,28 +2,42 @@ import React, { useEffect, useState } from "react";
 import { CiMusicNote1 } from "react-icons/ci";
 import { FaUser } from "react-icons/fa";
 import { IoEllipsisHorizontal } from "react-icons/io5";
+
 import "./Sidebar.css";
 
 function Sidebar({ isOpen }) {
   const buttons = [
     { label: "All Projects", icon: <CiMusicNote1 /> },
     { label: "My Projects", icon: <FaUser /> },
-    { label: "Discover Projects", icon: <IoEllipsisHorizontal /> },
+    { label: "Discover Projects", icon: <IoEllipsisHorizontal /> }
   ];
   const [selected, setSelected] = useState(buttons[0].label);
 
   return (
     <div className={`sidebar ${isOpen ? "sidebar-open" : "sidebar-closed"}`}>
-      {buttons.map((button) => (
-        <button
-          key={button.label}
-          onClick={() => setSelected(button.label)}
-          className={`sidebar-btn ${selected === button.label ? "selected" : ""}`}
-        >
-          <span className="icon">{button.icon}</span>
-          <a>{button.label}</a>
+      <div className ="top">
+        {buttons.map((button) => (
+          <button
+            key={button.label}
+            onClick={() => setSelected(button.label)}
+            className={`sidebar-btn ${selected === button.label ? "selected" : ""}`}
+          >
+            <span className="icon">{button.icon}</span>
+            <a>{button.label}</a>
+          </button>
+        ))}
+      </div>
+
+      {/* May be useful later, dont delete commented code */}
+      {/* <div className="bottom">
+        <button className="buttonoutline" style = {{ height: "40px" }} type="button" aria-label="Sign out">
+          <span className = "icon"><i className="fas fa-sign-out-alt"></i></span>
         </button>
-      ))}
+
+        <button className="buttonoutline" style = {{ height: "40px"}} type="button" aria-label="Sign out">
+          <span className = "icon"><i className="fas fa-cog"></i></span>
+        </button>
+      </div> */}
     </div>
   );
 }
